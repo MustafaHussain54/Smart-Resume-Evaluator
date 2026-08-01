29f300b097165c9561f3a90e68098b1ec69b855d
MustafaHussain54 <mustafahussain7828@gmail.com>
2026-08-01 23:35:08 +0530

    add some features to home page

--- Frontend/src/features/interview/pages/Home.jsx
+++ Frontend/src/features/interview/pages/Home.jsx
@@ -3,24 +3,22 @@ import "../styles/home.scss";
 const Home = () => {
   return (
     <main className="home">
-      
       <div className="interview-input-group">
         <div className="left">
-        <textarea
-          name="jobDescription"
-          id="jobDescription"
-          placeholder="Enter Job Description here..."
-        >
+          <textarea
+            name="jobDescription"
+            id="jobDescription"
+            placeholder="Enter Job Description here..."
+          ></textarea>
           
-        </textarea>
-        <div className="right">
-        <div className="input-group">
-          <label htmlFor="resume">Upload resume</label>
-          <input type="file" name="resume" id="resume" accept=".pdf" />
+          <div className="right">
+            <div className="input-group">
+              <label htmlFor="resume">Upload resume</label>
+              <input className="file-label" hidden type="file" name="resume" id="resume" accept=".pdf" />
+            </div>
+          </div>
         </div>
-      </div>
-      </div>
-      
+
         <div className="input-group">
           <label htmlFor="selfDescription">Self Description</label>
           <textarea
@@ -28,8 +26,9 @@ const Home = () => {
             id="selfDescription"
             placeholder="Describe yourself in a dew sentences..."
           ></textarea>
+          <button className="generate-btn">Generate interview Report</button>
         </div>
-        <button className="generate-btn">Generate interview Report</button>
+        
       </div>
     </main>
   );
